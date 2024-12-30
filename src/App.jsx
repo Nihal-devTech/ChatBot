937c6817ea31e6bfb87e1b72f6efaab1f7aa2635
Nihal Shah <shahnihal325@gmail.com>
2024-12-30 13:59:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useRef, useState } from "react";
+import ChatbotIcon from "./components/chatbotIcon";
+import ChatForm from "./components/ChatForm";
+import ChatMessage from "./components/ChatMessage";
+const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+  const chatBodyRef = useRef(null);
+
+  const updateChatHistory = (text) => {
+    setChatHistory((prev) => [
+      ...prev.filter((msg) => msg.text !== "thinking..."),
+      { role: "model", text },
+    ]);
+  };
+
+  const genResponse = async (history) => {
+    //Need to format chat history for API request
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
+    console.log(history);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history }),
+    };
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_API_URL,
+        requestOptions
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error.message || "Something went wrong");
+      }
+
+      const apiResponse = data.candidates[0].content.parts[0].text
+        .replace()
+        .trim();
+      updateChatHistory(apiResponse);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    chatBodyRef.current.scrollTo({
+      top: chatBodyRef.current.scrollHeight,
+      behaviour: "smooth",
+    });
+  }, [chatHistory]);
+  return (
+    <>
+      <div className="container">
+        <div className="chatbot-popup">
+          {/*ChatBot Header */}
+          <div className="chat-header">
+            <div className="header-info">
+              <ChatbotIcon />
+              <h2 className="logo-text">CHATBOT</h2>
+            </div>
+            <button className="material-symbols-outlined">
+              keyboard_arrow_down
+            </button>
+          </div>
+          <div ref={chatBodyRef} className="chat-body">
+            <div className="message bot-msg">
+              <ChatbotIcon />
+              <p className="message-text">Hey There, whats Your Mood</p>
+            </div>
+
+            {chatHistory?.map((chat, index) => (
+              <ChatMessage key={index} chat={chat} />
+            ))}
+          </div>
+          <div className="chat-footer">
+            <ChatForm
+              chatHistory={chatHistory}
+              setChatHistory={setChatHistory}
+              genResponse={genResponse}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
---
+++ src/components/ChatForm.jsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+
+const ChatForm = ({ chatHistory, setChatHistory, genResponse }) => {
+  //receiving prop
+  const inputRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userMessage = inputRef.current.value.trim(); //removing white spaces
+    if (!userMessage) return;
+    inputRef.current.value = "";
+    //updating chat history with the user message
+    setChatHistory((history) => [
+      ...history,
+      { role: "user", text: userMessage },
+    ]);
+
+    setTimeout(() => {
+      //Add a thinking message
+      setChatHistory((history) => [
+        ...history,
+        { role: "model", text: "thinking..." },
+      ]);
+      //call the function to generate bot response
+      genResponse([...chatHistory, { role: "user", text: userMessage }]);
+    }, 200);
+  };
+
+  return (
+    <>
+      <form action="#" className="chat-form" onSubmit={handleSubmit}>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Message..."
+          className="message-input"
+          required
+        />
+
+        <button className="material-symbols-outlined">Arrow_upward</button>
+      </form>
+    </>
+  );
+};
+
+export default ChatForm;
